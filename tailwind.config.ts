b972916b778c055f4aa23e025e69e9903e984bab
Anderson Lozano <adslozanov@gmail.com>
2025-04-09 11:00:56 -0500

    🔧 Colors, text font, and other details are configured to customize the tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,30 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "50dn": "#FFF0F9",
+        "100dn": "#FFE4F5",
+        "200dn": "#FFC9EC",
+        "300dn": "#FF9CDB",
+        "400dn": "#FF5FC1",
+        "500dn": "#FF31A8",
+        "600dn": "#F51486",
+        "700dn": "#D60068",
+        "800dn": "#B00454",
+        "900dn": "#920949",
+      },
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      keyframes: {
+        textReveal: {
+          '0%': { strokeDashoffset: '100' },
+          '50%': { fill: 'transparent' },
+          '80%': { strokeDashoffset: '0', stroke: 'black' },
+          '100%': { fill: 'black', strokeDashoffset: '0', stroke: 'black' }
+        }
+      },
+      animation: {
+        'text-reveal': 'textReveal 8s ease-in-out',
       },
     },
   },
