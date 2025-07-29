3af389c82d0adce18042924b88e3338fe51acec7
Anderson Lozano <adslozanov@gmail.com>
2025-07-29 10:41:23 -0500

    ✨ feat: add statistics data for happy clients and sales metrics

---
+++ src/seed/statistic.ts
@@ -0,0 +1,8 @@
+import { Statistic } from "@/interfaces";
+
+export const statistics: Statistic[] = [
+  { value: "544", label: "CLIENTES FELICES" },
+  { value: "119", label: "SEGUIDORES" },
+  { value: "611", label: "VENTAS" },
+  { value: "98%", label: "SATISFACCION" },
+];
